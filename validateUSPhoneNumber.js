17c5161515c6adf42fda4f1a0d0e3596f46ab24a
natsunoyoru <25550233+natsunoyoru97@users.noreply.github.com>
2018-06-03 14:48:45 +1000

    Update validateUSPhoneNumber.js

--- validateUSPhoneNumber.js
+++ validateUSPhoneNumber.js
@@ -2,5 +2,5 @@ function telephoneCheck(str) {
   if (str.length === 14 && str.charAt(0) !== '1') {
     return false;
   }
-  return  (/^1?\s?(\d{3})-?\s?(\d{3})-?\s?(\d{4})$/g.test(str) || /^1?\s?\((\d{3})-?\)\s?(\d{3})-?\s?(\d{4})$/g.test(str));
+  return (/^1?\s?(\d{3})-?\s?(\d{3})-?\s?(\d{4})$/g.test(str) || /^1?\s?\((\d{3})-?\)\s?(\d{3})-?\s?(\d{4})$/g.test(str));
 }
